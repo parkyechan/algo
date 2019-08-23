d9ae82b4b56e4e470e6c8014496769da48bf0b83
Park Yechan <evannalynch_@naver.com>
2019-08-23 16:21:15 +0900

    Upload

--- 11053/a.c
+++ 11053/a.c
@@ -1,23 +1,21 @@
 main(){
-  int N, flag[1001] = {}, max = 0;
-  int arr[1001] = {};
+  int N, arr[1001] = {}, flag, max = 0;
   scanf("%d", &N);
   for(int i=0; i<N; i++) scanf("%d", &arr[i]);
   for(int i=0; i<N; i++){
-    int big = 0, cnt = 0;
-    for(int j=i; j<N; j++){
-      // if(arr[j] > arr[i] && arr[j] >= big){
-      //   big = arr[j];
-      //   flag[i] = j - i - 1;
-      // }
-      if(arr[j] > arr[i]){
-        cnt++;
+    for(int j=i+1; j<N; j++){
+      int cnt = 1;
+      flag = arr[j];
+      for(int k=j; k<N; k++){
+        if(arr[k] > flag){
+          flag = arr[k];
+          cnt++;
+          // printf("%d ", arr[k]);
+        }
       }
+      // printf("\n");
       if(cnt > max) max = cnt;
     }
   }
-  // for(int i=0; i<N; i++){
-  //   if(flag[i] > max) max = flag[i];
-  // }
-  printf("%d", max);
+  printf("%d", max+1);
 }
