ec6396f4d1165c9ede420ed0cdbe60881972d377
Park Yechan <evannalynch_@naver.com>
2019-08-29 14:06:51 +0900

    Update a.c

--- 1316/a.c
+++ 1316/a.c
@@ -1,3 +1,25 @@
 main(){
-  
+  int N, sum = 0;
+  scanf("%d", &N);
+  for(int i=0; i<N; i++){
+    char arr[1001] = {};
+    int cnt = 0, alpa[30] = {}, flag = 0;
+    scanf("%s", arr);
+    while(1){
+      if(arr[cnt++] == '\0') break;
+    }
+    for(int j=0; j<cnt; j++) arr[j] -= 97;
+    for(int j=0; j<cnt; j++){
+      if(arr[j] == arr[j+1]){
+        continue;
+      }else{
+        alpa[arr[j]]++;
+      }
+      for(int k=0; k<26; k++){
+        if(alpa[k] > 1) flag++;
+      }
+    }
+    if(flag == 0) sum++;
+  }
+  printf("%d", sum);
 }
