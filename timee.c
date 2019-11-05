0574d6a1c3a5d4cd15323139953bc25892ded4ac
Park Yechan <evannalynch_@naver.com>
2019-11-05 19:33:51 +0900

    Update timee.c

--- timee.c
+++ timee.c
@@ -1,10 +1,10 @@
 main(){
   int a, b;
-  int sum = 0;
+  int sum = 0; 
   while(1){
     scanf("%d %d", &a, &b);
     a = a * 60;
-    sum += a + b; 
+    sum += a + b;
     int min =  sum / 60;
     int sec = sum % 60;
     printf("%d %d\n", min, sec);
